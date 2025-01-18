5a4522a7cc9d918b3d37538d9e5c24d57d3d9331
olik000 <oszlufik@agh.edu.pl>
2025-01-18 03:21:10 +0100

    Saving informations into Reservation db on Summary screen - printing informations for user

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, abort
-from database import db, Movie, Showing
+from database import db, Movie, Showing, Reservation
 from datetime import date, datetime
 from collections import defaultdict
 from flask import jsonify
 from flask import Flask, render_template, request, redirect, url_for, session
+import uuid
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = (
@@ -61,8 +62,59 @@ def personal(showing_id):
     showing = Showing.query.filter_by(showing_id=showing_id).first()
     return render_template("personal.html", showing=showing)
 
+
 @app.route("/showing/<showing_id>/personal/payment", methods=["GET", "POST"])
 def payment(showing_id):
+    
+    if request.method == "POST":
+        username = request.form.get("user-name")
+        email = request.form.get("user-email")
+        session["username"] = username
+        session["email"] = email
+        # Generowanie unikalnego numeru biletu
+        #session["ticket_number"] = str(uuid.uuid4())[:8]
+        session["ticket_number"] = 1
+        return redirect(url_for("payment", showing_id=showing_id))
+    # Obsługa GET
+    showing = Showing.query.filter_by(showing_id=showing_id).first()
+    username = session.get("username", "N/A")
+    email = session.get("email", "N/A")
+    return render_template("payment.html", showing=showing, username=username, email=email)
+
+
+@app.route("/showing/<showing_id>/personal/payment/summary")
+def summary(showing_id):
+    showing = Showing.query.filter_by(showing_id=showing_id).first()
+
+    username = session.get("username", "N/A")
+    email = session.get("email", "N/A")
+    ticket_number = session.get("ticket_number", "Nie ma biletu")
+    showing = Showing.query.filter_by(showing_id=showing_id).first()
+
+    # Dodaj rezerwację do bazy danych
+    new_reservation = Reservation(
+        showing_id=showing_id,
+        number_of_tickets=14,  # Możesz to dostosować w zależności od formularza
+        username= username,
+        email= email,
+        status="zrealizowana"  # Ustaw status na zrealizowana (lub inny)
+    )
+    db.session.add(new_reservation)
+    db.session.commit()
+
+    reservation = Reservation.query.filter_by(reservation_id=new_reservation.reservation_id).first()
+
+
+    return render_template(
+        "summary.html",
+        showing=showing,
+        reservation=reservation
+    )
+
+
+"""@app.route("/showing/<showing_id>/personal/payment", methods=["GET", "POST"])
+def payment(showing_id):
+    
     if request.method == "POST":
         username = request.form.get("user-name")
         email = request.form.get("user-email")
@@ -73,12 +125,12 @@ def payment(showing_id):
     showing = Showing.query.filter_by(showing_id=showing_id).first()
     username = session.get("username", "N/A")
     email = session.get("email", "N/A")
-    return render_template("payment.html", showing=showing, username=username, email=email)
+    return render_template("payment.html", showing=showing, username=username, email=email)"""
 
-@app.route("/showing/<showing_id>/personal/payment/summary")
+"""@app.route("/showing/<showing_id>/personal/payment/summary")
 def summary(showing_id):
     showing = Showing.query.filter_by(showing_id=showing_id).first()
-    return render_template("summary.html", showing=showing)
+    return render_template("summary.html", showing=showing)"""
 
 
 @app.errorhandler(404)
